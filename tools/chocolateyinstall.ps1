df1f179abc24f5635c61674f5511a47828f0bd9f
Kristian Zarębski <krizar312@gmail.com>
2023-04-17 17:33:28 +0100

    Add hash matching for icon

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,6 +1,7 @@
 ﻿
 $ErrorActionPreference = 'Stop';
 $checksum = "5A4E22A8EEC1F64EA91F3323E5FF1672E0466C7EDCC13D6F05B0629E3105BED6"
+$checksum_icon = "51CC10FCC171AD1F6B9798A8B8C359EA6D37C2A6DF904004155BB65AB8979C45"
 $repository = "AlbertBall/railway-dot-exe"
 
 Write-Host "Fetching version '$env:ChocolateyPackageVersion'"
@@ -23,5 +24,6 @@ $binaryDirectory = "$toolsDir\Release v$env:ChocolateyPackageVersion\Railway"
 Install-ChocolateyZipPackage @packageArgs
 Install-ChocolateyPath -PathToInstall $binaryDirectory -PathType "User"
 Get-WebFile -Url "https://raw.githubusercontent.com/AlbertBall/railway-dot-exe/master/railway_Icon.ico" -FileName $binaryDirectory\railway.ico
+Get-ChecksumValid -File $binaryDirectory\railway.ico -Checksum $checksum_icon -ChecksumType 'sha256'
 Install-ChocolateyShortcut -ShortcutFilePath "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Railway Operation Simulator.lnk" -TargetPath "$binaryDirectory\railway.exe" -IconLocation $binaryDirectory\railway.ico
 Update-SessionEnvironment
