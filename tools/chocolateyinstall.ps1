fa4ab9ba5c1477ed2e1af25f8d448d38880dc44b
Kristian Zarębski <krizar312@gmail.com>
2024-02-13 17:36:51 +0000

    Redesign install script for different folder structure

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿
 $ErrorActionPreference = 'Stop';
-$checksum_64 = "31f17860bb54505590192f0a5425ff510552b38adcebe3610f26f813124ab869"
-$checksum_32 = "8f58d434aff414f034f38b6bb389e838634b433fe964a3ffb81005b74132420b"
+$checksum_64 = "5e6a4f39ba3aebd73f88fe7d8893e11b749afb5aa76e271c9588871a05bae46f"
+$checksum_32 = "c7d697f3057f3a0f624d06372adb19f8a9cd888853015e1d38771f74dbb8aa1f"
 $checksum_icon = "51CC10FCC171AD1F6B9798A8B8C359EA6D37C2A6DF904004155BB65AB8979C45"
 $repository = "AlbertBall/railway-dot-exe"
 
@@ -10,6 +10,8 @@ Write-Host "Fetching version '$env:ChocolateyPackageVersion'"
 $download_url_32 = "https://github.com/$repository/releases/download/v$env:ChocolateyPackageVersion/RailOS32-v$env:ChocolateyPackageVersion.zip"
 $download_url_64 = "https://github.com/$repository/releases/download/v$env:ChocolateyPackageVersion/RailOS64-v$env:ChocolateyPackageVersion.zip"
 $toolsDir = "$env:ProgramFiles\Railway_Operation_Simulator"
+$target_dir_32 = "$toolsDir\RailOS32-v$env:ChocolateyPackageVersion"
+$target_dir_64 = "$toolsDir\RailOS64-v$env:ChocolateyPackageVersion"
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
@@ -24,10 +26,23 @@ $packageArgs = @{
   validExitCodes= @(0, 3010, 1605, 1614, 1641)
 }
 
+
+
 $binaryDirectory = "$toolsDir\Railway"
 $exe_path_32 = "$binaryDirectory\RailOS32.exe"
 $exe_path_64 = "$binaryDirectory\RailOS64.exe"
+
 Install-ChocolateyZipPackage @packageArgs
+
+if("$env:ChocolateyForceX86") {
+  Move-Item -Path "$target_dir_32\Railway" -Destination $binaryDirectory
+  Remove-Item -LiteralPath $target_dir_32 -Force -Recurse
+}
+else {
+  Move-Item -Path "$target_dir_64\Railway" -Destination $binaryDirectory
+  Remove-Item -LiteralPath $target_dir_64 -Force -Recurse
+}
+
 Install-ChocolateyPath -PathToInstall $binaryDirectory -PathType "User"
 Get-WebFile -Url "https://raw.githubusercontent.com/AlbertBall/railway-dot-exe/master/railway_Icon.ico" -FileName $binaryDirectory\railway.ico
 Get-ChecksumValid -File $binaryDirectory\railway.ico -Checksum $checksum_icon -ChecksumType 'sha256'
